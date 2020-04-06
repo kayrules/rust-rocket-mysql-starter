03e90d1f0070b5321fd724eed306f288fa2c9249
Khairulnizam <mohd.khairulnizam.md@rhbgroup.com>
2020-04-06 09:32:41 +0800

    nullable-id.sh check boundary for id

--- scripts/nullable-id.sh
+++ scripts/nullable-id.sh
@@ -2,5 +2,5 @@
 _srcpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
 
 diesel print-schema > $_srcpath/src/schema.rs
-/usr/bin/sed -i .bak 's/id -> Integer,/id -> Nullable<Integer>,/' $_srcpath/src/schema.rs
+/usr/bin/sed -i .bak 's/[[:<:]]id -> Integer,/id -> Nullable<Integer>,/' $_srcpath/src/schema.rs
 rm $_srcpath/src/schema.rs.bak
